6fd60dda1e717dfb5f98b5ed4595655643b8b0c7
ibads8 <ibads8@users.noreply.github.com>
2022-06-23 18:10:58 -0600

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          {" "}
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>
+            {" "}
+            This project was coded by Eu and is{" "}
+            <a
+              href="https://github.com/ibads8/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://app.netlify.com/sites/genuine-pixie-8e22b3/overview"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
