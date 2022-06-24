ed7ef38c9658f4ac92c44ff598aabe6b0bdb1e7f
ibads8 <ibads8@users.noreply.github.com>
2022-06-24 13:02:49 -0600

    Edited phonetics component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,12 +4,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        alt="Image"
-      >
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
       <span className="text">{props.phonetic.text}</span>
