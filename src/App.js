be51ba5a899bba0209bcf13aa1dd29c20b9a27bc
ibads8 <ibads8@users.noreply.github.com>
2022-06-23 19:27:59 -0600

    Update CSS and title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="App-header">Dictionary</header>
         <main>
           {" "}
           <Dictionary />
